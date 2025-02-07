338326c2fab3fab51a6a3a5656b41ae78ec6ed6b
Amar Shah <ashah12@br014.ib.bridges2.psc.edu>
2025-02-07 15:30:17 -0500

    fixed run_clause_length.py

--- run_clause_lengths.py
+++ run_clause_lengths.py
@@ -22,7 +22,7 @@ OUTPUT_FILES = ["global_clauses.txt", "proof.pr"]
 
 
 # Select random files
-files = ["7f7109dce621ef361a72b3e8cee9a962-Break_unsat_06_07.xml.cnf"] + [f for f in os.listdir(INPUT_DIR) if f.endswith(".cnf") and f != "f7109dce621ef361a72b3e8cee9a962-Break_unsat_06_07.xml.cnf"]
+files = [f for f in os.listdir(INPUT_DIR) if f.endswith(".cnf")]
 # random.shuffle(files)
 # selected_files = files[:N]
 
@@ -36,13 +36,18 @@ def process_file(file_name):
     
     start_time = time.time()
     try:
-        subprocess.run(cmd, shell=True, timeout=TIMEOUT, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.run(cmd, shell=True, timeout=TIMEOUT, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         elapsed_time = time.time() - start_time
         print(f"The file {file_name} terminated!")
     except subprocess.TimeoutExpired:
         print(f"The file {file_name} timed out!")
-    except subprocess.CalledProcessError:
-        print(f"The file {file_name} returned a non-zero exit status!")   
+    except subprocess.CalledProcessError as result:
+        if result.returncode == 10:
+            print(f"The file {file_name} returned SAT!")   
+        elif result.returncode == 20:
+            print(f"The file {file_name} returned UNSAT!")   
+        else:
+            print(f"The file {file_name} returned a non-zero exit status!")   
 
         # return [file_name, elapsed_time]  # Return immediately in case of failure
     file_path = f"results/proofs/{file_name}.proof.pr"
@@ -55,24 +60,6 @@ def process_file(file_name):
 
     print(f"We have finished on file {file_name}")
 
-    # Copy output files
-    # for output_file in OUTPUT_FILES:
-    #     output_file = file_name + "." + output_file
-    #     print(f"We have the file: {output_file}")
-    #     if os.path.exists(output_file):
-    #         dest_folder = CLAUSES_DIR if output_file.endswith("global_clauses.txt") else PROOFS_DIR
-    #         print(f"Moving {output_file} to {dest_folder}/{file_name}.{output_file}")
-    #         shutil.move(output_file, os.path.join(dest_folder, f"{file_name}.{output_file}"))
-
-    # # Analyze clause lengths
-    # clause_lengths = []
-    # global_clauses_path = os.path.join(CLAUSES_DIR, f"{file_name}.global_clauses.txt")
-
-    # if os.path.exists(global_clauses_path):
-    #     with open(global_clauses_path, 'r') as f:
-    #         clause_lengths = [len(line.split()) for line in f if line.strip()]
-    
-    # return [file_name, elapsed_time] + clause_lengths
 
 # Parallel execution
 proc_num = 20
